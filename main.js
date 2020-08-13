d04b24fcc2a02dd0e8a50baff9d53812b20e3347
mcelenk <celenk.must@gmail.com>
2020-08-13 14:03:07 +0200

    repositioning debugging

--- main.js
+++ main.js
@@ -388,10 +388,10 @@
                 skipArrowHead: (p == 0),
                 arrowColor: arrowColor
             });
-            if (p == 1) {
-                console.log('CENTER SET AT ', c[0], ' ' , c[1]);
-                view.setCenter(new Point(c[0], c[1]));
-            }
+            //if (p == 1) {
+            //    console.log('CENTER SET AT ', c[0], ' ' , c[1]);
+            //    view.setCenter(new Point(c[0], c[1]));
+            //}
         }
         sortCircles();
     }
