e4a39a3c7e305496d75c9bc5a3867dace04d702a
mcelenk <celenk.must@gmail.com>
2020-11-01 14:40:07 +0100

    snapp param update

--- functions/get-data.js
+++ functions/get-data.js
@@ -31,7 +31,7 @@ exports.handler = function (event, context, callback) {
                 endFrame: 670,
             },
             snap: {
-                startFrame: 150,
+                startFrame: 750,
             },
         },
     };
