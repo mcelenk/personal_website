c2ed9f42db8995945d82a43929e2f3b206d08c40
mcelenk <celenk.must@gmail.com>
2024-12-25 21:33:09 +0300

    file name casing change

---
+++ src/functions/checkAndInsertGameUser.ts
@@ -0,0 +1,52 @@
+import { Handler } from '@netlify/functions';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGODB_URI!);
+
+const handler: Handler = async (event, context) => {
+    // Parse the item from the request body
+    const item = JSON.parse(event.body!);
+
+    if (!item || !item.userId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Missing item or item ID' }),
+        };
+    }
+
+    try {
+        await client.connect();
+        const database = client.db('AntiyoyCloneDB');
+        const collection = database.collection('GameUser');
+
+        // Find and update the item, or insert if it doesn't exist
+        const result = await collection.findOneAndUpdate(
+            { userId: item.userId }, // Query to find the item
+            { $setOnInsert: item }, // Update to apply if the item is not found
+            { returnDocument: 'after', upsert: true } // Options: return the new document, upsert if not found
+        );
+
+        if (result?.lastErrorObject?.updatedExisting) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: 'Item already exists', item: result.value }),
+            };
+        } else {
+            return {
+                statusCode: 201,
+                body: JSON.stringify({ message: 'Item inserted successfully', item: result?.value }),
+            };
+        }
+    } catch (error) {
+        console.error('Error processing request:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' }),
+        };
+    } finally {
+        // Ensure the client is closed after the operation
+        await client.close();
+    }
+};
+
+export { handler };
