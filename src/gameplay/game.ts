aa18e164199c63bffc0cf176d52f1c5ffd1d941f
mcelenk <celenk.must@gmail.com>
2025-01-03 20:00:07 +0300

    still testing mapGeneratorgit add .

--- src/gameplay/game.ts
+++ src/gameplay/game.ts
@@ -42,7 +42,7 @@ export class Game {
             const column: Array<Hex> = [];
             for (let y = 0; y < mapData.height; y++) {
                 const active = mapData.grid[x][y];
-                column.push(new Hex(y, x, active, 0, Obj.NONE, -1));
+                column.push(new Hex(y, x, active, 1, Obj.NONE, 0));
             }
             gameData.field.push(column);
         }
