776d6d488cc34a15751b2b93d86d533c3dec9cf1
mcelenk <celenk.must@gmail.com>
2025-01-07 17:21:36 +0300

    Introducing real users retrieval for game creation

---
+++ src/functions/getAllUsers.ts
@@ -0,0 +1,37 @@
+import { Handler } from '@netlify/functions';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGODB_URI!);
+
+const handler: Handler = async (event, _context) => {
+    const userId = event.queryStringParameters?.userId;
+    if (!userId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Missing userId query parameter' }),
+        };
+    }
+
+    try {
+        await client.connect();
+        const database = client.db('AntiyoyCloneDB');
+        const collection = database.collection('GameUser');
+
+        const records = await collection.find({ userId: { $ne: userId } }).toArray();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(records),
+        };
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' }),
+        };
+    } finally {
+        await client.close();
+    }
+};
+
+export { handler };
