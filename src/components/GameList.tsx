87dd0ee6c30d8650b0913b4f9800ee9f35723db0
mcelenk <celenk.must@gmail.com>
2025-01-14 16:57:48 +0300

    Introducing toast notification for ended games, inital commit

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import { useAuth } from './AuthContext';
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/GameList.css';
 import '../styles/Common.css';
+import { ObjectId } from 'mongodb';
 
 interface Game {
     gameId: string,
@@ -10,9 +13,17 @@ interface Game {
     isCurrent: boolean,
 }
 
+interface Notification {
+    _id: ObjectId,
+    gameName: string,
+    message: string
+}
+
 const GameList: React.FC = () => {
     const [games, setGames] = useState<Array<Game>>([]);
+    const [notifications, setNotifications] = useState<Array<Notification>>([]);
     const [loading, setLoading] = useState<boolean>(true);
+    const [gamesLoaded, setGamesLoaded] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
     const navigate = useNavigate();
     const { user } = useAuth();
@@ -30,12 +41,50 @@ const GameList: React.FC = () => {
                 setError('Error fetching game data');
                 console.error(err);
             } finally {
-                setLoading(false);
+                setGamesLoaded(true);
             }
         };
 
         fetchGames();
     }, [user]);
+
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            if (gamesLoaded) {
+                try {
+                    const response = await fetch(`/.netlify/functions/getGameNotifications?userId=${user.sub}`);
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    const data = await response.json();
+                    setNotifications(data.notifications);
+
+                    const notificationIds = notifications.map((x: Notification) => x._id);
+                    notifications.forEach((notification) => { toast(notification.message, { autoClose: 5000 }); });
+                    markNotificationsAsRead(notificationIds);
+
+                } catch (err) {
+                    setError('Error fetching notifications data');
+                    console.error(err);
+                } finally {
+                    setLoading(false);
+                }
+            }
+        };
+
+        const markNotificationsAsRead = async (notificationIds: Array<ObjectId>) => {
+            console.log('Mark notification as read is called with this array : ');
+            notificationIds.forEach(x => console.log(x));
+            // await fetch('/api/notifications/read', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({ notificationIds }),
+            // });
+        };
+        fetchNotifications();
+    }, [user, gamesLoaded]);
 
     if (loading) {
         return <div>Loading...</div>;
@@ -59,6 +108,7 @@ const GameList: React.FC = () => {
                     ))}
                 </ul>
             </div>
+            <ToastContainer />
         </>
     );
 };
