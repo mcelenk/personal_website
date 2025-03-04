7dc46dc9ef04366b864751a52fc10dba7964adfb
mcelenk <celenk.must@gmail.com>
2025-03-04 19:39:04 +0300

    trying out different numCircles values

--- src/drawing_with_ft/dataProvider.ts
+++ src/drawing_with_ft/dataProvider.ts
@@ -28,7 +28,7 @@ export class DefaultDataProvider implements IDataProvider {
                 snappedToTip: true,
                 stopped: false,
                 initialZoom: 200, //process.env.START_ZOOM, //400.0,
-                numCircles: 150,
+                numCircles: 200,
                 fill: true,
                 prefferedDt: 0.0009332,
                 roundCompletedInfo: {
