189689306ada67e4d70e01d3be1f5ae90e91aac5
mcelenk <celenk.must@gmail.com>
2025-03-06 13:53:33 +0300

    increasing numCircles back to 250

--- src/drawing_with_ft/dataProvider.ts
+++ src/drawing_with_ft/dataProvider.ts
@@ -28,7 +28,7 @@ export class DefaultDataProvider implements IDataProvider {
                 snappedToTip: true,
                 stopped: false,
                 initialZoom: 200, //process.env.START_ZOOM, //400.0,
-                numCircles: 225,
+                numCircles: 250,
                 fill: true,
                 prefferedDt: 0.0009332,
                 roundCompletedInfo: {
