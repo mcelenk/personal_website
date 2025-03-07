3ab128826dcaa7ba535df7d379267a7249a0efe7
mcelenk <celenk.must@gmail.com>
2025-03-07 17:44:45 +0300

    Removing capture ability, and updating portfolio to use the captured gif

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -3,9 +3,6 @@ import { useParams, Navigate, useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 import ConfirmModal from './ConfirmModal';
 import { Game } from '../gameplay/game';
-
-import gifshot from 'gifshot';
-
 import '../styles/Common.css';
 import '../styles/GameScreen.css';
 
@@ -228,53 +225,6 @@ const GameScreen: React.FC = () => {
         }
     }, [serializationData]);
 
-    const captureAnimation = () => {
-        const combinedCanvas = document.createElement('canvas');
-        const combinedContext = combinedCanvas.getContext('2d', { willReadFrequently: true })!;
-        combinedCanvas.width = canvasBackRef.current!.width;
-        combinedCanvas.height = canvasBackRef.current!.height;
-        combinedContext.drawImage(canvasBackRef.current!, 0, 0);
-        combinedContext.drawImage(canvasFrontRef.current!, 0, 0);
-
-        let canvas = combinedCanvas;
-        const frames: string[] = [];
-        const duration = 6000; // Capture duration in milliseconds
-        const frameRate = 5; // Lower the frame rate to reduce size (e.g., 5 frames per second)
-        const totalFrames = (duration / 1000) * frameRate;
-
-        const captureFrame = (frameCount: number) => {
-            if (frameCount >= totalFrames) {
-                gifshot.createGIF(
-                    {
-                        images: frames,
-                        gifWidth: canvas.width / 4, // Reduce resolution by half
-                        gifHeight: canvas.height / 4, // Reduce resolution by half
-                        interval: 1 / frameRate,
-                        numWorkers: 2,
-                        numFrames: totalFrames,
-                        sampleInterval: 10, // Increase sample interval to reduce size
-                    },
-                    (obj) => {
-                        if (!obj.error) {
-                            const image = obj.image;
-                            const a = document.createElement('a');
-                            a.href = image!;
-                            a.download = 'animation.gif';
-                            a.click();
-                        }
-                    }
-                );
-                return;
-            }
-            combinedContext.drawImage(canvasBackRef.current!, 0, 0);
-            combinedContext.drawImage(canvasFrontRef.current!, 0, 0);
-            frames.push(combinedCanvas.toDataURL('image/png'));
-            setTimeout(() => captureFrame(frameCount + 1), 1000 / frameRate);
-        };
-
-        captureFrame(0);
-    };
-
     if (isLoading) {
         return <div>Loading...</div>;
     }
@@ -291,7 +241,6 @@ const GameScreen: React.FC = () => {
             <div className="button-container absolute-positioning">
                 <button className="common-button" onClick={() => navigate('/games')}>Back to Game List</button>
                 <button className='common-button' disabled={buttonsDisabled} onClick={handleResign}>Resign</button>
-                <button onClick={captureAnimation}>Capture Animation</button>
             </div>
             <canvas ref={canvasBackRef} id="back" width={window.innerWidth} height={window.innerHeight}></canvas>
             <canvas ref={canvasFrontRef} id="front" width={window.innerWidth} height={window.innerHeight}></canvas>
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -19,18 +19,18 @@ const Portfolio: React.FC = () => {
             imageSrc: '/assets/dwft.gif',
             title: 'Drawing With Fourier Transform',
             description: <>Inspired by 3Blue1Brown's <a href="https://www.youtube.com/watch?v=spUNpyF58BY" target="_blank" rel="noopener noreferrer">videos</a>,
-                and a will to learn a bit about <a href="http://paperjs.org/" target="_blank" rel="noopener noreferrer">PaperJS</a>,
+                and a will to learn more about <a href="http://paperjs.org/" target="_blank" rel="noopener noreferrer">PaperJS</a>,
                 I created this project to visualize how a complex shape can be represented by a sum of simpler shapes using Fourier Transform.</>,
             technologies: ['PaperJS', 'HTML Canvas', 'Typescript'],
             navigateTo: '/project1', // Define the navigation path
         },
         {
-            imageSrc: 'path/to/your/image2.png', // Replace with your image paths
-            title: 'Project 2',
+            imageSrc: '/assets/antiyoy.gif',
+            title: 'AntiYoy Online',
             description: <>This is my take on implementing a turn-based strategy game. Inspired by a classic Android game, I developed an online version for two players.
                 This web-based version offers seamless and engaging gameplay, capturing the essence of the original.
                 Original game developed by <a href="https://play.google.com/store/apps/details?id=yio.tro.antiyoy.android" target="_blank" rel="noopener noreferrer">yiotro.</a></>,
-            technologies: ['HTML Canvas', 'Serverless Functions', 'MongoDB'],
+            technologies: ['Typescript', 'HTML Canvas', 'Serverless Functions', 'MongoDB'],
             navigateTo: '/games',
         },
         // Add more portfolio items as needed
